b9d4eeb572eb2a15fb9820324d1d6a2fb7db8be2
EBSmash <53373788+EBSmash@users.noreply.github.com>
2022-07-23 14:40:47 -0400

    1.0

---
+++ src/main/java/com/lambda/util/FriendManager.java
@@ -0,0 +1,38 @@
+package com.lambda.util;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FriendManager {
+    static ArrayList<String> usernames = new ArrayList<>();
+
+    public static List<String> getRawPastebin(String pasteKey){
+
+        try {
+            URL url = new URL("https://pastebin.com/raw/" + pasteKey);
+            Scanner scanner = new Scanner(url.openStream());
+            while (scanner.hasNext()){
+                usernames.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return usernames;
+
+    }
+
+
+    public static boolean isFriendly(String username) {
+        for (String name : getRawPastebin("8aJ7KgE8")) {
+            if(name.equalsIgnoreCase(username)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
