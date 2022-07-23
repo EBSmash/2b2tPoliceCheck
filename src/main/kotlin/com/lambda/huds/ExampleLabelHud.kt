764b0408f51f0fb84cfdfd3e43c2cb8454c16c11
EBSmash <53373788+EBSmash@users.noreply.github.com>
2022-07-23 15:05:25 -0400

    newline

--- src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
+++ src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
@@ -16,10 +16,10 @@ internal object ExampleLabelHud : PluginLabelHud(
     override fun SafeClientEvent.updateText() {
         for(player in mc.world.playerEntities) {
             if(FriendManager.isFriendly(player.name)) {
-                displayText.add(player.name, ColorHolder(0,255,0))
+                displayText.add(player.name + "\n", ColorHolder(0,255,0))
             }
             else{
-                displayText.add(player.name, ColorHolder(255,0,0))
+                displayText.add(player.name + "\n", ColorHolder(255,0,0))
 
             }
         }
