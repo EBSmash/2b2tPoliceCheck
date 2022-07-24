05de6678762bd28eb4257a647084e71cd895aecc
EBSmash <53373788+EBSmash@users.noreply.github.com>
2022-07-24 14:31:34 -0400

    show everyone else in red

--- src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
+++ src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
@@ -15,11 +15,16 @@ internal object ExampleLabelHud : PluginLabelHud(
 
     override fun SafeClientEvent.updateText() {
         displayText.add("Goon Squad:")
+        displayText.currentLine++
         for(player in mc.world.playerEntities) {
             if(FriendManager.isFriendly(player.name)) {
                 displayText.add(player.name , ColorHolder(0,255,0))
                 displayText.currentLine++
 
+            }
+            else {
+                displayText.add(player.name, ColorHolder(255, 0, 0))
+                displayText.currentLine++
             }
         }
 
